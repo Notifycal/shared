f45f8ac8add3c4e4e973b1f97566f2e823a3b1a3
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-01-30 14:21:52 +0100

    fix: more renaming and file organization (#20)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-export * from './api/api-common';
 export * from './api/authentication';
 export * from './api/calendar';
 export * from './api/reminder-config';
+export * from './api/responses';
 export * from './api/user';
 export * from './common';
