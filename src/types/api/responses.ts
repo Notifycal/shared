8e7f8a3e0ed8409f483ad1eada4df5aeb3ea6fb6
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-01-30 16:03:33 +0100

    fix: ts extension madness with relative imports

--- src/types/api/responses.ts
+++ src/types/api/responses.ts
@@ -1,4 +1,4 @@
-import { AuthenticationResponse } from './authentication';
+import { AuthenticationResponse } from './authentication.js';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type BodyContent = Record<string, any> | Array<unknown>;
