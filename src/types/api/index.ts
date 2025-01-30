3b6a1b4ef62c36cf0eff427f9315cdf8738232c1
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-01-30 15:04:33 +0100

    fix: import issue where stuff in api can't be found

---
+++ src/types/api/index.ts
@@ -0,0 +1,5 @@
+export * from './authentication';
+export * from './calendar';
+export * from './reminder-config';
+export * from './responses';
+export * from './user';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,2 @@
-export * from './api/authentication';
-export * from './api/calendar';
-export * from './api/reminder-config';
-export * from './api/responses';
-export * from './api/user';
+export * from './api';
 export * from './common';
