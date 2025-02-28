6c30bff6d371b6669b4af708a1e2fb97a2690fa7
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-02-28 16:30:31 +0100

    wip. circular dependency

--- src/schemas/common.ts
+++ src/schemas/common.ts
@@ -8,3 +8,15 @@ export const emailSchema = z.string().email().brand('Email');
 export const idpIdSchema = z.string().brand('IdpId');
 export const dateTimeSchema = z.string().datetime().brand('DateTime');
 export const timeZoneSchema = z.string().brand('TimeZone');
+
+export const phoneContactSchema = z.object({
+  type: z.literal('phone'),
+  identifier: z.string().brand('PhoneNumber')
+});
+
+export const rcsContactSchema = z.object({
+  type: z.literal('rcs'),
+  identifier: z.string().brand('RCSSenderId')
+});
+
+export const contactSchema = z.discriminatedUnion('type', [rcsContactSchema, phoneContactSchema]);
--- src/schemas/reminder-template.ts
+++ src/schemas/reminder-template.ts
@@ -3,6 +3,7 @@ import { z, ZodLiteral } from 'zod';
 import { templateMap } from '@templates';
 import { TemplateId } from '@types';
 import { calendarSchema } from './calendar';
+import { contactSchema } from './common';
 
 export const templateSchema = z.string().max(160).brand('InterpolatedTemplate');
 const templateSchemaIds = Object.values(templateMap)
@@ -14,6 +15,9 @@ export const templateReferenceSchema = z.union(templateSchemaIds);
 
 export const reminderConfigSchema = z.object({
   calendars: z.array(calendarSchema.extend({ templateId: templateReferenceSchema })).min(1),
-  businessName: z.string().min(1).brand('BusinessName'),
-  businessAddress: z.string().min(1).brand('BusinessAddress')
+  business: z.object({
+    name: z.string().min(1).brand('BusinessName'),
+    address: z.string().min(1).brand('BusinessAddress'),
+    contactDetails: contactSchema
+  })
 });
