2f7e67cdc146a9438d0fd7e442bc934a40c3fa7f
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-03-06 15:05:25 +0100

    fix prettier

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,3 @@
 export * from './calendar';
 export * from './common';
 export * from './reminder';
-
--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -2,4 +2,3 @@ export type * from './authentication';
 export type * from './calendar';
 export type * from './responses';
 export type * from './user';
-
