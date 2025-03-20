efdc151e06c80f9ded31ce782a789203f295851b
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-03-20 17:50:58 +0100

    fix!: move back assets to its original location cause they are time consuming to extract. Split phone and language data from flags

--- src/i18n/common.ts
+++ src/i18n/common.ts
@@ -1,29 +1,21 @@
-import flagEn from '@assets/en.png';
-import flagEs from '@assets/es.png';
 import type { CountryCode, LanguageCode, LanguageData, PhoneData } from '@types';
 
 // https://en.wikipedia.org/wiki/E.164
 // where it states there is a mapping of country codes to dial codes - with some exceptions
-export const phoneData: Record<CountryCode, PhoneData> = {
+export const phoneByCountry: Record<CountryCode, Omit<PhoneData, 'image'>> = {
   ES: {
     label: 'Spain',
     code: 'ES',
-    image: flagEs,
     phoneDetails: { numberMask: /^(?:\+34\s?)?[67]\d{8}$/, dialCode: '+34' }
   },
   EN: {
     label: 'United Kingdom',
     code: 'EN',
-    image: flagEn,
     phoneDetails: { numberMask: /^(?:\+44\s?|0)7\d{9}$/, dialCode: '+44' }
   }
 };
 
-export const countryDialCodeMatrix: Record<CountryCode, string> = Object.fromEntries(
-  Object.entries(phoneData).map(([key, value]) => [key as CountryCode, value.phoneDetails.dialCode])
-) as Record<CountryCode, string>;
-
-export const languageData: Record<LanguageCode, LanguageData> = {
-  es: { label: 'Spanish', code: 'es', image: flagEs },
-  en: { label: 'English', code: 'en', image: flagEn }
+export const languageByLanguageCode: Record<LanguageCode, Omit<LanguageData, 'image'>> = {
+  es: { label: 'Spanish', code: 'es' },
+  en: { label: 'English', code: 'en' }
 };
