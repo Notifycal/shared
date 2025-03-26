aaa472c88c2953bf58ddfc7964fab03cb89988c3
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-03-26 19:56:16 +0100

    fix: gb country code. Add language to template schema to be able to edit the field

--- src/schemas/reminder.ts
+++ src/schemas/reminder.ts
@@ -1,13 +1,12 @@
-import { templateSchemaIds } from '@templates';
+import { templateSelectionSchema } from '@templates';
 import { z } from 'zod';
 import { calendarSchema } from './calendar';
 import { contactSchema } from './common';
 
 export const templateSchema = z.string().max(160).brand('InterpolatedTemplate');
-export const templateReferenceSchema = z.union(templateSchemaIds);
 
 export const reminderConfigSchema = z.object({
-  calendars: z.array(calendarSchema.extend({ templateId: templateReferenceSchema })).min(1),
+  calendars: z.array(calendarSchema.extend({ template: templateSelectionSchema })).min(1),
   business: z.object({
     name: z.string().min(1).brand('BusinessName'),
     address: z.string().min(1).brand('BusinessAddress'),
