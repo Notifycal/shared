74b14b525290fb8d7f4ebd82a767d9d2764534e3
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-04-17 15:31:06 +0200

    feat!: add config to user model. That introduced a circular dependency that had to be fixed. Added models for phone contact

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,5 @@
 export * from './calendar';
 export * from './common';
+export * from './contact';
 export * from './i18n';
-export * from './reminder';
+
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,6 @@
-import type { Email, IdpId, UnixTimestamp, UserId } from '../common';
+import type { z } from 'zod';
+import type { Email, IdpId, UnixTimestamp, UserId } from './common';
+import type { reminderConfigSchema } from './reminder';
 
 // When time comes, append IdpName with | 'idpName2'
 export type IdpName = 'google.com';
@@ -15,8 +17,12 @@ export interface Identity<IdpName> extends BaseIdentity {
 
 export type UserStatus = 'banned' | 'onboarding' | 'live';
 
+// This should really be defined in @schemas but this is an exception to resolve a circular dependency between @schemas, @templates and @types
+export type ReminderConfig = z.infer<typeof reminderConfigSchema>;
+
 export interface User<TIdpName extends IdpName> extends Identity<TIdpName> {
   lastSignInAt: UnixTimestamp;
   signedUpAt: UnixTimestamp;
   userStatus: UserStatus;
+  config?: ReminderConfig;
 }
