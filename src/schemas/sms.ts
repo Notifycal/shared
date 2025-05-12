148485d5253b6203dba7ecec0ca8d9fb4c59b70b
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-05-12 14:47:21 +0200

    feat: add schema for SMS (body or field) (#128)

---
+++ src/schemas/sms.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const SMS_CHARACTER_REGEX = /^[a-zA-Z0-9\s.,!?'"():;\-_]*$/;
+export const SMS_CHARACTER_LIMIT = 160;
+
+type SmsValidationMessages = Partial<{
+  invalidType: string;
+  regex: string;
+}>;
+
+const zodMessageOrUndefined = (message?: string): { message: string } | undefined => {
+  return message ? { message: message } : undefined;
+};
+
+export const createSmsContentSchema = (messages: SmsValidationMessages = {}): z.ZodString =>
+  z
+    .string(zodMessageOrUndefined(messages.invalidType))
+    .regex(SMS_CHARACTER_REGEX, zodMessageOrUndefined(messages.regex));
