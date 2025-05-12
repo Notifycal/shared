0b329a13c8a8d04c36f96942666a120b4c7c1c6e
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-05-12 14:45:12 +0200

    fix: make templates sms-proof. Also remove `senderDetails` from `demoReminderPayloadSchema` (#127)

--- src/schemas/reminder.ts
+++ src/schemas/reminder.ts
@@ -1,9 +1,7 @@
 import { z } from 'zod';
 import { dateTimeSchema, timeZoneSchema } from './common';
-import { receiverSchema } from './contact';
 
 export const demoReminderPayloadSchema = z.object({
-  receiverContact: receiverSchema,
   startTime: z.object({
     dateTime: dateTimeSchema,
     timeZone: timeZoneSchema
