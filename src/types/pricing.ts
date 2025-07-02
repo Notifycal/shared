46e5ce579bcd03b09ed80b7c02063143b45d9622
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-07-02 21:56:41 +0200

    feat: extract topup model from backend (#175)

--- src/types/pricing.ts
+++ src/types/pricing.ts
@@ -1,8 +1,14 @@
+import type { productRawSchema, tierDetailsRawSchema, tierMapSchema, topupMapSchema } from '@schemas';
 import type z from 'zod';
-import type { tierMapSchema, tierDetailsRawSchema } from '@schemas';
 
 export type TierId = keyof z.input<typeof tierMapSchema>;
 export type Tier = z.infer<typeof tierDetailsRawSchema>;
 export type TierMap = {
   [K in TierId]: Tier;
+};
+
+export type TopupId = keyof z.input<typeof topupMapSchema>;
+export type Topup = z.infer<typeof productRawSchema>;
+export type TopupMap = {
+  [K in TopupId]: Topup;
 };
