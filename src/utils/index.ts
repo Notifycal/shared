7da73e8476b041e20d761ca3945fe122be1f8729
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-02 23:22:21 +0200

    rename

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export { bundleSizePlugin as handleBundleSizePlugin } from './bundle-size-plugin';
+export { bundleSizePlugin } from './bundle-size-plugin';
 export * from './case.js';
 export { default as sleep } from './sleep.js';
