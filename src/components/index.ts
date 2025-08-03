a97a29a2891135594d2ec161d19301023affb78f
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-04 00:08:10 +0200

    code related to tiers and extending tiers info. Static landing will need it too

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { TierCard, type TierInfo, type TierInfoWithIcon } from './TierCard/TierCard';
 export { TierFeatures } from './TierFeatures/TierFeatures';
 export { TierSelection } from './TierSelection/TierSelection';
+
