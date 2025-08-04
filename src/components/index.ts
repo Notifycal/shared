18f5b020f65e657e167ad1d0a056cee6ff4e28f6
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-04 17:32:44 +0200

    prettier

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { TierCard, type TierInfo, type TierInfoWithIcon } from './TierCard/TierCard';
 export { TierFeatures } from './TierFeatures/TierFeatures';
 export { TierSelection } from './TierSelection/TierSelection';
-
