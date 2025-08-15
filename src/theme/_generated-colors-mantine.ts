e3ee00e4bec2236e389f07b3c923a4832d1c05d0
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-16 00:23:40 +0200

    add generated files (for now)

---
+++ src/theme/_generated-colors-mantine.ts
@@ -0,0 +1,66 @@
+/* This file was generated automatically, do not edit it */
+
+import type { MantineColorsTuple } from '@mantine/core';
+
+export const colors: Record<string, MantineColorsTuple> = {
+  primary: [
+    'var(--color-primary-50)',
+    'var(--color-primary-100)',
+    'var(--color-primary-200)',
+    'var(--color-primary-300)',
+    'var(--color-primary-400)',
+    'var(--color-primary-500)',
+    'var(--color-primary-600)',
+    'var(--color-primary-700)',
+    'var(--color-primary-800)',
+    'var(--color-primary-900)'
+  ],
+  secondary: [
+    'var(--color-secondary-50)',
+    'var(--color-secondary-100)',
+    'var(--color-secondary-200)',
+    'var(--color-secondary-300)',
+    'var(--color-secondary-400)',
+    'var(--color-secondary-500)',
+    'var(--color-secondary-600)',
+    'var(--color-secondary-700)',
+    'var(--color-secondary-800)',
+    'var(--color-secondary-900)'
+  ],
+  accent1: [
+    'var(--color-accent1-50)',
+    'var(--color-accent1-100)',
+    'var(--color-accent1-200)',
+    'var(--color-accent1-300)',
+    'var(--color-accent1-400)',
+    'var(--color-accent1-500)',
+    'var(--color-accent1-600)',
+    'var(--color-accent1-700)',
+    'var(--color-accent1-800)',
+    'var(--color-accent1-900)'
+  ],
+  accent2: [
+    'var(--color-accent2-50)',
+    'var(--color-accent2-100)',
+    'var(--color-accent2-200)',
+    'var(--color-accent2-300)',
+    'var(--color-accent2-400)',
+    'var(--color-accent2-500)',
+    'var(--color-accent2-600)',
+    'var(--color-accent2-700)',
+    'var(--color-accent2-800)',
+    'var(--color-accent2-900)'
+  ],
+  neutral: [
+    'var(--color-neutral-50)',
+    'var(--color-neutral-100)',
+    'var(--color-neutral-200)',
+    'var(--color-neutral-300)',
+    'var(--color-neutral-400)',
+    'var(--color-neutral-500)',
+    'var(--color-neutral-600)',
+    'var(--color-neutral-700)',
+    'var(--color-neutral-800)',
+    'var(--color-neutral-900)'
+  ]
+};
