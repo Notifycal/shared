9d558aeac0bd0c1a91bfdb962b7c5f7b7d6d95bf
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-08-18 22:11:45 +0200

    feat: service config plugin (#270)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { bundleSizePlugin } from './bundle-size-plugin';
 export * from './case.js';
+export { ourServiceConfigPlugin } from './our-service-config-plugin';
 export { serviceConfigFactory } from './service-config-factory';
 export { default as sleep } from './sleep.js';
