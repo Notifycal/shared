92989168ed333cc68aa3ad94ed00d579e9978bc9
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-18 17:41:59 +0200

    prettier

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,3 @@ export { bundleSizePlugin } from './bundle-size-plugin';
 export * from './case.js';
 export { serviceConfigFactory } from './service-config-factory';
 export { default as sleep } from './sleep.js';
-
