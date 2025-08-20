fb83a64ee94a02062d5d7413d4a0f2622fbfc5e6
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-20 14:32:56 +0200

    feat: cookie consent config

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export { bundleSizePlugin } from './bundle-size-plugin';
 export * from './case.js';
+export * from './cookie-consent-config';
 export { ourServiceConfigPlugin } from './our-service-config-plugin';
 export { serviceConfigFactory } from './service-config-factory';
 export { default as sleep } from './sleep.js';
+
