0bedbb37aa9cf1df66eb5ab58e27188e5d68c225
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-08-22 10:45:20 +0200

    fix: let cookie stuff bve alone so backend does not explode when buil… (#284)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export { bundleSizePlugin } from './bundle-size-plugin';
 export * from './case.js';
-export * from './cookie-consent-config';
 export { ourServiceConfigPlugin } from './our-service-config-plugin';
 export { serviceConfigFactory } from './service-config-factory';
 export { default as sleep } from './sleep.js';
