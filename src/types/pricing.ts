b39f7ba4e9406e97806c8f93a36bccb1927f5ac7
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-12-01 13:15:12 +0100

    code from backend

--- src/types/pricing.ts
+++ src/types/pricing.ts
@@ -1,5 +1,6 @@
 import type {
   customerPortalFlowTypeSchema,
+  freeTrialTierId,
   productRawSchema,
   tierDetailsRawSchema,
   tierMapSchema,
@@ -7,11 +8,9 @@ import type {
 } from '@schemas';
 import type z from 'zod';
 
-export const freeTrialTierId = 'good-trial' as const;
 export type FreeTrialTierId = typeof freeTrialTierId;
 export type TierId = keyof z.infer<typeof tierMapSchema>;
 export type Tier = z.infer<typeof tierDetailsRawSchema>;
-export type TierWithFreeTrial = Omit<Tier, 'id'> & { id: TierId | FreeTrialTierId };
 export type TierMap = {
   [K in TierId]: Tier;
 };
