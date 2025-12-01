559487776b26c79b835ac933d0e9b05fc0d053f1
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-12-01 12:57:37 +0100

    move types from backend

--- src/types/pricing.ts
+++ src/types/pricing.ts
@@ -7,8 +7,11 @@ import type {
 } from '@schemas';
 import type z from 'zod';
 
+export const freeTrialTierId = 'good-trial' as const;
+export type FreeTrialTierId = typeof freeTrialTierId;
 export type TierId = keyof z.infer<typeof tierMapSchema>;
 export type Tier = z.infer<typeof tierDetailsRawSchema>;
+export type TierWithFreeTrial = Omit<Tier, 'id'> & { id: TierId | FreeTrialTierId };
 export type TierMap = {
   [K in TierId]: Tier;
 };
